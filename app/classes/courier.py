be6321586901397aaeffdf99e763cbaf34f0b40c
StasHunter92 <stashunter92@gmail.com>
2022-12-11 16:46:03 +0300

    Refactor code

--- app/classes/courier.py
+++ app/classes/courier.py
@@ -7,29 +7,29 @@ from app.classes.request import Request
 # Courier class to move goods between storages
 class Courier:
     def __init__(self, request: Request):
-        self.request = request
+        self.__request = request
 
     def make_order(self) -> None:
         """
         Takes a request and move goods between storages \n
         :return: None
         """
-        self.request.get_from().remove(self.request.get_product(), self.request.get_amount())
-        print(f"\nКурьер забрал {self.request.get_amount()} {self.request.get_product()} "
-              f"из {self.request.get_name_from()}")
+        self.__request.get_from().remove(self.__request.get_product(), self.__request.get_amount())
+        print(f"\nКурьер забрал {self.__request.get_amount()} {self.__request.get_product()} "
+              f"из {self.__request.get_name_from()}")
         time.sleep(1)
 
-        print(f"Курьер везет {self.request.get_amount()} {self.request.get_product()} "
-              f"из {self.request.get_name_from()} в {self.request.get_name_to()} ")
+        print(f"Курьер везет {self.__request.get_amount()} {self.__request.get_product()} "
+              f"из {self.__request.get_name_from()} в {self.__request.get_name_to()} ")
         time.sleep(1)
 
-        self.request.get_to().add(self.request.get_product(), self.request.get_amount())
-        print(f"Курьер доставил {self.request.get_amount()} {self.request.get_product()} "
-              f"в {self.request.get_name_to()} ")
+        self.__request.get_to().add(self.__request.get_product(), self.__request.get_amount())
+        print(f"Курьер доставил {self.__request.get_amount()} {self.__request.get_product()} "
+              f"в {self.__request.get_name_to()} ")
 
     def return_order(self) -> None:
         """
         Returns the order if destination is unavailable
         :return: None
         """
-        self.request.get_from().add(self.request.get_product(), self.request.get_amount())
+        self.__request.get_from().add(self.__request.get_product(), self.__request.get_amount())
